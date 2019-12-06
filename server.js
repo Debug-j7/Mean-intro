08301b4940edbbe14cf3340194bb613e92a451f9
EJabulani <jabulanishany@gmail.con>
2019-12-06 10:28:46 +0200

    creating a nodeJs http Server

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('THIS IS MY FIRST RESPONSE');
+
+});
+
+server.listen(process.env.PORT || 3000);
